7bd1ad9003e7a5b15ff958bc73fbe70c5f8f8d6a
yoshitsugu_fujii <ishikurasakura@gmail.com>
2018-07-13 16:23:54 +0900

    cli test

--- lib/generate_schemafile.rb
+++ lib/generate_schemafile.rb
@@ -1,4 +1,7 @@
+p 'sssssssssssssss'
+
 require "generate_schemafile/version"
+require "generate_schemafile/cli"
 
 module GenerateSchemafile
   # Your code goes here...
---
+++ lib/generate_schemafile/cli.rb
@@ -0,0 +1,9 @@
+require "thor"
+module GenerateSchemafile
+  class CLI < Thor
+    desc "generate schemafile for ridgepole"
+    def generate
+      p 'hogehoge'
+    end
+  end
+end
